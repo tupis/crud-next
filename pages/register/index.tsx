dae69d8da43bbd930beb71d6da41ba7a04f6d8dd
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-10-18 15:47:46 -0300

    feat: create forms

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,9 +1,36 @@
+import Head from "next/head";
 import { NextPage } from "next";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import { useState } from "react";
 
 const Register: NextPage = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   return (
     <>
-      <h1>Registro</h1>
+      <Head>
+        <title>Registrar-se</title>
+      </Head>
+      <form>
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button>Registrar-se</Button>
+      </form>
     </>
   );
 };
