2674f89e9bf9a4b38b5bc18498a3d2ff4c7528c4
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-10-19 12:38:27 -0300

    feat: create function register

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -3,11 +3,21 @@ import { NextPage } from "next";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import { useState } from "react";
+import UserServices from "../../services/userServices";
 
 const Register: NextPage = () => {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const { register } = UserServices;
+    await register({ email, password, name }).then((response) =>
+      console.log(response)
+    );
+  };
+
   return (
     <>
       <Head>
@@ -29,7 +39,7 @@ const Register: NextPage = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <Button>Registrar-se</Button>
+        <Button onClick={(e) => handleSubmit(e)}>Registrar-se</Button>
       </form>
     </>
   );
