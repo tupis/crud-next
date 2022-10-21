f0af11aac4e87e6e3d2508c99e69f636d12c0517
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-10-21 13:54:18 -0300

    feat: union update services

--- services/userServices.ts
+++ services/userServices.ts
@@ -3,11 +3,9 @@ import axios from "axios";
 const UserServices = {
   register: async (params: object) => await axios.post("/api/register", params),
   login: async (params: object) => await axios.post("/api/login", params),
-  deleteUser: async (id: string) => await axios.post(`/api/user/name/${id}`),
-  updateName: async (params: object, id: string) =>
-    await axios.post(`/api/user/name/${id}`, params),
-  updatePassword: async (params: object, id: string) =>
-    await axios.post(`/api/user/name/${id}`, params),
+  deleteUser: async (id: string) => await axios.post(`/api/user/delete/${id}`),
+  updateUser: async (params: object, id: string) =>
+    await axios.put(`/api/user/update/${id}`, params),
 };
 
 export default UserServices;
