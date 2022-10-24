4dafb6ce9ac8c8307d8881b08e328e36cab53466
João Tupinambá <joaoh.tupinamba@gmail.com>
2022-10-24 14:11:06 -0300

    fix: change method post to delete

--- services/userServices.ts
+++ services/userServices.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const UserServices = {
   register: async (params: object) => await axios.post("/api/register", params),
   login: async (params: object) => await axios.post("/api/login", params),
-  deleteUser: async (id: string) => await axios.post(`/api/user/delete/${id}`),
+  deleteUser: async (id: string) =>
+    await axios.delete(`/api/user/delete/${id}`),
   updateUser: async (params: object, id: string) =>
     await axios.put(`/api/user/update/${id}`, params),
 };
